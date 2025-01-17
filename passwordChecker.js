6803d599eba073eea6f7465e508afcfa2b5aaf6b
Alexandre01Dev <altata01@hotmail.com>
2025-01-17 16:43:45 +0100

    add: function hasNumber in passwordChecker

---
+++ passwordChecker.js
@@ -0,0 +1,28 @@
+
+// Check if the password is valid
+function passwordChecker(password) {
+    return moreThan8Characters(password) && hasSpecialCharacters(password);
+}
+
+
+// Check if the password is more than 8 characters
+function moreThan8Characters(password) {
+    return password.length >= 8;
+}
+
+// Check if the password has special characters
+function hasSpecialCharacters(password) {
+    return /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password);
+}
+
+// Check if the password has Number
+function hasNumber(password) {
+    return /[0-9]/.test(password);
+}
+
+
+module.exports = {
+    passwordChecker,
+    moreThan8Characters,
+    hasSpecialCharacters
+};
